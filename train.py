feeb6fdb200ee2d5d2a5982f1273c67dfaa97b24
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-09-26 11:01:23 +0800

    debugging

--- train.py
+++ train.py
@@ -105,21 +105,36 @@ def train(data_loader):
     accumulation_steps = 32 // args.batch_size
     optimizer.zero_grad()
     for idx, (img, segm) in enumerate(tqdm(data_loader)):
-        img = img.cuda()
-        segm = segm.cuda()
-        outputs = model(img)
-        loss = criterion(outputs, segm)
-        (loss/accumulation_steps).backward()
-        clipping_value = 1.0
-        torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
-        if (idx + 1 ) % accumulation_steps == 0:
-            optimizer.step() 
-            optimizer.zero_grad() 
-        total_loss += loss.item()
-        
-        # clear the GPU cache against overmemory
-        if (idx + 1 ) % args.clearing_steps == 0:
-            torch.cuda.empty_cache()
+        try:
+            img = img.cuda()
+            segm = segm.cuda()
+            outputs = model(img)
+            loss = criterion(outputs, segm)
+            (loss/accumulation_steps).backward()
+            clipping_value = 1.0
+            torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+            if (idx + 1 ) % accumulation_steps == 0:
+                optimizer.step() 
+                optimizer.zero_grad() 
+            total_loss += loss.item() 
+        except RuntimeError as e:
+            if 'out of memory' in str(e):
+                # clear the GPU cache against overmemory
+                print('| WARNING: ran out of memory')
+                torch.cuda.empty_cache()
+                img = img.cuda()
+                segm = segm.cuda()
+                outputs = model(img)
+                loss = criterion(outputs, segm)
+                (loss/accumulation_steps).backward()
+                clipping_value = 1.0
+                torch.nn.utils.clip_grad_norm_(model.parameters(), clipping_value)
+                if (idx + 1 ) % accumulation_steps == 0:
+                    optimizer.step() 
+                    optimizer.zero_grad() 
+                total_loss += loss.item() 
+            else:
+                raise e
             
     return total_loss/len(data_loader)
 
