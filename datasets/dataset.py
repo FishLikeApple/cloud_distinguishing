46059dc62259fd4cac0a918d78216d9019e1de62
apple_fish <31738680+FishLikeApple@users.noreply.github.com>
2019-10-14 16:59:10 +0800

    Update dataset.py

--- datasets/dataset.py
+++ datasets/dataset.py
@@ -107,6 +107,7 @@ class SteelDataset(Dataset):
             mask = mask.float()
         else:
             mask = mask*torch.tensor([1, 2, 3, 4], dtype=torch.float32)
+            assert mask.shape == [1, 256, 1600, 4] # make sure of the shape
             mask, _ = torch.max(mask, -1) 
             mask = mask.long()
         return img, mask
